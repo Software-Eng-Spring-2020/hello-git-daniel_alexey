14deb686c3c2710964bea4fb4e207319d2358584
AlexeyAlive <alexeyalive13@gmail.com>
2020-04-15 22:20:31 +0300

    my name Alexey, added printline

--- src/main/java/il/ac/haifa/cs/sweng/HelloGit/hello.java
+++ src/main/java/il/ac/haifa/cs/sweng/HelloGit/hello.java
@@ -3,6 +3,9 @@ package il.ac.haifa.cs.sweng.HelloGit;
 public class hello {
 	
 	public static void name() {
-		System.out.println("First commit Alexey");
+		System.out.println("Hello Git");
+		
+		
+		System.out.println("my name is Alexey");
 	}
 }
